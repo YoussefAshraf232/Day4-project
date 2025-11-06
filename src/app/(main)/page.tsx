80e97b9100d57ccddbdd782f4cf284f63332b38d
YoussefAshraf <ashrafyossef65@gmail.com>
2025-11-06 12:36:41 +0200

    clear git clear
    first commit
    
    f

---
+++ src/app/(main)/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function page() {
+    return (
+        <div className=' h-full w-full bg-amber-100 flex items-center justify-center'>
+            <header className='text-5xl font-bold bg-amber-300 p-4 rounded-2xl'>
+            Random User
+            <br />
+            </header>
+        </div>
+    )
+}
